ff0d05ca381ad84d5712649529015f859f0b0601
sonia rani <sonia1183.cse19@chitkara.edu.in>
2022-02-02 10:36:39 +0530

    preloader-11

---
+++ src/contents/main_page/main.js
@@ -0,0 +1,9 @@
+import './main.css';
+function Main() {
+  return (
+    <div classname="faded">
+        <h1>hii</h1>
+    </div>
+  );
+}
+export default Main;
--- src/contents/preloader/preloader.js
+++ src/contents/preloader/preloader.js
@@ -1,11 +1,33 @@
 import GIF from '../preloader/preloader_gif.gif';
-import '../preloader/preloader.css'
-import React from "react";
+import Main from '../main_page/main';
+import React, { useEffect, useState } from "react";
+import "./preloader.css";
+
 function PreLoader() {
+  const [, setData] = useState([]);
+  const [loading, setloading] = useState(undefined);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setloading(true);
+        });
+    }, 3000);
+  }, []);
+
   return (
-    <div className='gif'>
-      <img src={GIF} alt=""/>
-      </div>
+    <>
+          {!loading ? (
+           <div className='gif spinner'>
+                 <img src={GIF} alt=""/>
+                 </div>
+          ) : (
+            <Main/>
+          )}
+        </>
   );
 }
 
